bf27d5253905ee9044245c2ff15617284b83fe04
LaoQi <me@madao.dev>
2026-07-22 07:46:21 +0800

    theme: tango

---
+++ examples/themes/tango.lua
@@ -0,0 +1,21 @@
+return {
+    fg = "#eeeeec",
+    bg = "#2e3436",
+    cursor = "#eeeeec",
+    palette = {
+        "#000000", "#cc0000", "#4e9a06", "#c4a000",
+        "#3465a4", "#75507b", "#06989a", "#d3d7cf",
+        "#555753", "#ef2929", "#8ae234", "#fce94f",
+        "#729fcf", "#ad7fa8", "#34e2e2", "#eeeeec",
+    },
+    osd = {
+        bar_bg = "#000000",
+        active_bg = "#3465a4",
+        inactive_bg = "#2e3436",
+        active_fg = "#eeeeec",
+        inactive_fg = "#555753",
+        csd_btn_bg = "#555753",
+        csd_close_bg = "#cc0000",
+        csd_btn_fg = "#eeeeec",
+    },
+}
